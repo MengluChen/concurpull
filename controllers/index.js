af6ea3b1d882f0d63a48279543c5992c90d94460
Jinfan Duan <jinfan.duan@gmail.com>
2015-06-11 10:23:37 -0700

    [expense] initial connection to concur and couchbase

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,8 @@
 'use strict';
-
+var app =  require('../index');
+var concur = require('concur-platform');
+var couchbase = require('couchbase');
+var myCluster = new couchbase.Cluster(app.kraken.get('couchbase:cluster'));
 
 var IndexModel = require('../models/index');
 
@@ -10,11 +13,42 @@ module.exports = function (router) {
 
 
     router.get('/', function (req, res) {
-        
-        
-        res.render('index', model);
-        
-        
-    });
 
+
+        res.render('index', model);
+
+
+    });
+    router.get('/concur', function (req, res) {
+
+        var options = {
+            oauthToken: app.kraken.get('concur:oauthToken')
+        };
+
+        concur.user.get(options)
+            .then(function(data) {
+                var myBucket = myCluster.openBucket('test_concur');
+                myBucket.insert("test"+(new Date()).getTime(), data, function(err, res1) {
+                    if (err) {
+                        console.log('operation failed', err);
+                        /*
+                         operation failed { [Error: The key does not exist on the server] code: 13 }
+                         */
+                        return;
+                    }
+
+                    console.log('success!', res1);
+                });
+                res.end(JSON.stringify(data, null, 4));
+
+            })
+            .fail(function(error) {
+                // Error will contain the error returned.
+                res.end( JSON.stringify(error, null, 4));
+            });
+
+       // res.end(output);
+
+
+    });
 };
